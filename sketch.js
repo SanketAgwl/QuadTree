2263e705504e9ad6b54162769128d4aa0437d9de
Sanket <lnmbqrry@lnmiit.ac.in>
2021-02-20 11:47:47 +0530

    points functionality added

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+ let qt;
  function setup()
 {
     createCanvas(800, 800);
@@ -5,13 +6,26 @@
     let boundary= new Rectangle(400, 400, 400, 400);
     qt = new QuadTree(boundary, 4);
 
-    for(i=0; i<Math.floor(Math.random() * 100000) ; i++)
+    /*for(i=0; i<Math.floor(Math.random() * 100000) ; i++)
     {
         let p =new Point(random(width), random(height));
         qt.insert(p);
     }
-
-    background(0);
-    qt.show();
-
+*/background(0);
+    
 }
+
+function draw()
+{
+    if(mouseIsPressed)
+    {
+        let pt=new Point(mouseX, mouseY);
+        qt.insert(pt);
+    }
+
+    qt.show();
+}
+
+
+
+    
